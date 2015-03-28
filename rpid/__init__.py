7121e3c41660b23b99cd37b3fd6d661b6b4c62a6
Jim Rybarski <jim@rybarski.com>
2015-03-27 22:45:34 -0500

    added PID controller

--- rpid/__init__.py
+++ rpid/__init__.py
@@ -1,6 +1,12 @@
 from itertools import cycle
 import smbus
 import time
+import numpy as np
+import logging
+
+log = logging.getLogger()
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.DEBUG)
 
 
 class Relay(object):
@@ -94,8 +100,59 @@ class TemperatureProgram(object):
         return self._hold_temp if self._hold_temp else False
 
 
-# def install(self, relay):
-#     with open("/sys/class/gpio/export", "w") as f:
-#         f.write(relay.number + "\n")
-#     with open(relay.path("direction"), "w") as f:
-#         f.write("out")
+class PID:
+    def __init__(self, kp=1.0, ki=1.0, kd=1.0, accumulated_error_min=-20, accumulated_error_max=20):
+        assert accumulated_error_min < 0 < accumulated_error_max
+        self._kp = kp
+        self._ki = ki
+        self._kd = kd
+        self._previous_errors = []
+        self._accumulated_error = 0.0
+        self._accumulated_error_max = accumulated_error_max
+        self._accumulated_error_min = accumulated_error_min
+        self._set_point = 25.0
+
+    def update(self, current_value):
+        log.debug("\n\n")
+        error = self._set_point - current_value
+        log.debug("Error: %s" % error)
+        self._update_previous_errors(current_value, error)
+        self._update_accumulated_error(error)
+        p = self._kp * error
+        log.debug("Proportional: %s" % p)
+        i = self._accumulated_error * self._ki
+        log.debug("Integral: %s" % i)
+        d = self._calculate_error_derivative() * self._kd
+        log.debug("Derivative: %s" % d)
+        total = p + i + d
+        log.debug("PID total: %s" % total)
+        return total
+
+    def _calculate_error_derivative(self):
+        # finds the slope of the least squares regression for the last five error measurements
+        a = np.vstack([np.array(self._previous_errors), np.ones(len(self._previous_errors))]).T
+        b = np.arange(float(len(self._previous_errors)))
+        slope = np.linalg.lstsq(a, b)[0][0]
+        return slope
+
+    def _update_previous_errors(self, current_value, error):
+        if not self._previous_errors:
+            self._previous_errors = [float(current_value) for i in range(5)]
+        self._previous_errors.pop(0)
+        self._previous_errors.append(error)
+
+    def _update_accumulated_error(self, error):
+        # Add the current error to the accumulated error
+        self._accumulated_error += error
+        # Ensure the value is within the allowed limits
+        self._accumulated_error = min(self._accumulated_error, self._accumulated_error_max)
+        self._accumulated_error = max(self._accumulated_error, self._accumulated_error_min)
+        log.debug("Accumulated error: %s" % self._accumulated_error)
+
+    @property
+    def set_point(self):
+        return self._set_point
+
+    @set_point.setter
+    def set_point(self, temperature):
+        self._set_point = float(temperature)
