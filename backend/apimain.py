836e5a86b5e2090f1a60674efc6a5563ce787e1a
Jim Rybarski <jim@rybarski.com>
2015-05-26 15:55:16 +0000

    add delays so RPI isn't hammered at boot

--- backend/apimain.py
+++ backend/apimain.py
@@ -1,4 +1,6 @@
 from server import *
+import time
 
 if __name__ == "__main__":
+    time.sleep(330)
     app.run(host="127.0.0.1", port=8089)
--- backend/backendmain.py
+++ backend/backendmain.py
@@ -1,6 +1,8 @@
 from rpid import Data, Output, TemperatureProbe, TemperatureController
+import time
 
 if __name__ == "__main__":
+    time.sleep(300)
     probe = TemperatureProbe()
     output = Output()
     data_provider = Data()
