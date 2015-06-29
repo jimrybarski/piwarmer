c046e1e63b4a433cf3ea363f11a8c58389dcd211
jim <jim@rybarski.com>
2015-06-29 12:21:42 -0500

    resolves #49

--- backend/rpid/runner.py
+++ backend/rpid/runner.py
@@ -145,6 +145,6 @@ class ProgramRunner(BaseRunner):
             self._heater.heat(round_data.duty_cycle)
 
             # update the API data so the frontend can know what's happening
-            self._api_data.seconds_left = round_data.seconds_left
+            self._api_data.time_left = program.convert_seconds_to_hhmmss(round_data.seconds_left)
             self._api_data.update_temperature(round_data.current_temperature)
             self._api_data.update_setting(round_data.desired_temperature)
