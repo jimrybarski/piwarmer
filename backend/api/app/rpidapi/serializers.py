1b93d8f6528213a7086d36ab03e1d2ffe8c2386d
jim <jim@rybarski.com>
2015-07-13 16:07:38 -0500

    #67 started django site

---
+++ backend/api/app/rpidapi/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+
+
+class ScientistSerializer(ModelSerializer):
+    pass
+
+
+class DriverSerializer(ModelSerializer):
+    pass
+
+
+class ProgramSerializer(ModelSerializer):
+    pass
