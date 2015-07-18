82fa94d6ff47b1bdbfe043640f5b0316f0b3c4b1
Jim Rybarski <jim@rybarski.com>
2015-07-18 09:00:11 -0500

    #70: shows users, creates users

--- frontend/common.js
+++ frontend/common.js
@@ -1,16 +1,17 @@
 var API_URL = 'temp.control:8001'
 
-function http(endpoint, verb, func){
+function http(endpoint, verb, d, func){
     $.ajax({
       type: verb,
       crossDomain: true,
       url: "http://" + API_URL + "/" + endpoint,
       contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(d),
       dataType: "json",
-      success: function(data){
-          func(data);
+      success: function(return_data){
+          func(return_data);
       },
-      failure: function(data) {
+      failure: function(return_data) {
         alert("The API could not be reached.");
       },
     });
--- frontend/home.js
+++ frontend/home.js
@@ -1,5 +1,30 @@
-data = http('user', 'GET', function(data){
-    for (i=0; i<data.length; i++) {
-        alert(data[i]['name'] + data[i]['id'])
-    }
+$(document).ready(function(){
+    $("#create_new_user").click( function()
+        {
+            name = $("#new_username").val();
+            if (name.length > 0) {
+                http('user', 'POST', {'name': name}, function(data){
+                    $("#new_username").val('');
+                    location.reload();
+                })
+            }
+            else {
+                alert("Please enter a name.")
+            }
+        }
+    );
+
+    http('user', 'GET', null, function(data){
+        buttons = ''
+        for (i=0; i<data.length; i++) {
+            line = "<input type='submit' onclick='window.location.href="
+            line += '"/program?user='
+            line += data[i]['id']
+            line += '"\' value="'
+            line += data[i]['name']
+            line += '"><br>'
+            buttons += line;
+        }
+    $("#users").html(buttons);
+    });
 });
