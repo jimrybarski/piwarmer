617b159ff826c093ef8121071308bfc1c321e449
Jim Rybarski <jim@rybarski.com>
2015-07-20 15:49:54 -0500

    resolves #77

--- backend/api/rpidapi/views.py
+++ backend/api/rpidapi/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import detail_route
 from . import serializers, models
 from interface import APIData
 from pprint import pprint
--- frontend/program/manage/manage_program.js
+++ frontend/program/manage/manage_program.js
@@ -1,6 +1,34 @@
+function display_step(data) {
+    if (data['mode'] == 'set') {
+        return 'Set to ' + data['temperature'] + '&deg;C for ' + data['duration'] + ' seconds'
+    }
+    if (data['mode'] == 'linear') {
+        return 'Ramp from ' + data['start_temperature'] + '&deg;C to ' + data['end_temperature'] + '&deg;C over ' + data['duration'] + ' seconds.'
+    }
+    if (data['mode'] == 'hold') {
+        return 'Hold at ' + data['temperature'] + '&deg;C'
+    }
+    if (data['mode'] == 'repeat') {
+        return 'Repeat ' + data['num_repeats'] + ' times'
+    }
+}
+
 $(document).ready(function(){
     program_id = get_id('id');
     http('program/' + program_id, 'GET', null, function(response) {
-        $("#program_details").html(response['steps']);
+        console.log(response);
+        step_data = JSON.parse(response['steps']);
+        console.log(step_data);
+        step_keys = [];
+        steps = ""
+        for (var key in step_data) {
+            step_keys.push(key);
+        }
+        step_keys.sort()
+        for (var key in step_keys) {
+            steps += '<tr><td>' + step_keys[key] + '</td><td>' + display_step(step_data[step_keys[key]]) + '</td></tr>'
+        }
+
+        $("#program_details").html(steps);
     });
 });
