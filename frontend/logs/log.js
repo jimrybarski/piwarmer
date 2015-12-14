fdf84677db498fb95c1e15059be5eb45f66f78e6
Jim Rybarski <jim@rybarski.com>
2015-12-14 17:40:07 -0600

    #43: retrieves log file names

--- frontend/logs/log.js
+++ frontend/logs/log.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
-    http("logs", 'GET', null, function(data){
-        $("#logs").html(data);
+   http("logs", 'GET', null, function(data) {
+
+       var output = "";
+
+       for (i=0; i<Object.keys(data).length; i++) {
+           output += data[i] + "<br>";
+       }
+
+       $("#logs").html(output);
+  
+   });
 });
