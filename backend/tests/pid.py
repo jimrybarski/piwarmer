20e8d91e0b6235a712516479157e0b2401328366
jim <jim@rybarski.com>
2015-12-22 00:18:04 -0600

    #134 more derivative tests

--- backend/tests/pid.py
+++ backend/tests/pid.py
@@ -10,3 +10,15 @@ class PIDTests(unittest.TestCase):
     def test_calculate_derivative_default_values(self):
         d = self.pid._calculate_derivative(1.0, self.pid._past_errors)
         self.assertEqual(d, 0.0)
+
+    def test_calculate_derivative_some_values(self):
+        d = self.pid._calculate_derivative(1.0, [2.0, 4.0, 6.0, 8.0, 10.0, 12.0])
+        self.assertAlmostEqual(d, 2.0)
+
+    def test_calculate_derivative_big_factor(self):
+        d = self.pid._calculate_derivative(3.7, [2.0, 4.0, 6.0, 8.0, 10.0, 12.0])
+        self.assertAlmostEqual(d, 7.4)
+
+    def test_negative_derivative(self):
+        d = self.pid._calculate_derivative(1.0, [12.0, 10.0, 8.0, 6.0, 4.0, 2.0])
+        self.assertAlmostEqual(d, -2.0)
